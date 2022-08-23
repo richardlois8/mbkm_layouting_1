63f859657032ba20151063f5daad1a14be699cc9
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-08-23 22:39:31 +0700

    Latihan dan tugas layouting

---
+++ app/src/main/java/com/example/topic4/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.topic4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tugas_tiga)
+
+//        val btnLinier = findViewById<Button>(R.id.btnLinier)
+//        val btnRelative = findViewById<Button>(R.id.btnRelative)
+//        btnLinier.setOnClickListener {
+//            val intent = Intent(this, LinearLayout::class.java)
+//            startActivity(intent)
+//        }
+//        btnRelative.setOnClickListener {
+//            val intent = Intent(this, RelativeLayout::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
